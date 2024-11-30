b897947a5b28f3c61e00ff3cdfa9f1379b710162
Nikhil <nikhilgupta932003@gmail.com>
2024-12-01 01:05:01 +0530

    added the backend which include db connection error handler and server . lets do the rest

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from "express" 
+import cors from "cors"
+import dotenv from "dotenv"
+import { dbconnection } from "./database/dbconnection.js"
+import { errorMiddleware } from "./error/error.js";
+
+const app=express();
+dotenv.config({path:"./config/config.env"})
+
+app.use(
+    cors({
+        origin:[process.env.FRONTEND_URL],
+        method:["post"],
+        credentials:true,
+    })
+);
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+dbconnection();
+app.use(errorMiddleware)
+
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+import app from "./app.js"
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port ${process.env.PORT}`);
+    
+})
