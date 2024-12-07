5607381398a1c9190c338d8c045cf325a6ee4365
Nikhil <nikhilgupta932003@gmail.com>
2024-12-07 20:18:46 +0530

    added backend

--- cafe/src/components/Reservation.jsx
+++ cafe/src/components/Reservation.jsx
@@ -32,7 +32,7 @@ const Reservation = () => {
     try {
       const { data } = await axios.post(
         // "http://localhost:4000/api/v1/reservation/send",
-        "cafe-site-sandy.vercel.app",
+        "cafe-site-omega.vercel.app",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
